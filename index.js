b20f2b4c0054d493c802e5216d22ab3ce21eb261
Griko Nibras <git@griko.id>
2022-04-27 01:40:09 +0700

    feat(src): add list route

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ async function handler(/** @type {FetchEvent} */ event) {
   const url = new URL(event.request.url);
   if (url.pathname == "/") return Response.redirect(BASE_URL, 307);
 
-  const slug = url.pathname.split("/")[1];
-  if (slug) {
-    const redirectUrl = await REDIRECTS.get(slug, { cacheTtl: 60 * 5 });
-    if (redirectUrl) return Response.redirect(redirectUrl, 307);
+  if (url.pathname == "/_") {
+    const list = await REDIRECTS.list();
+    return new Response(list.keys.reduce((acc, val) => acc + val.name + "\n", ""));
   }
 
-  return Response.redirect(`${BASE_URL}/${slug}`, 307);
+  const slug = url.pathname.split("/")[1];
+  const redirectUrl = await REDIRECTS.get(slug, { cacheTtl: 60 * 5 });
+  if (!redirectUrl) return Response.redirect(`${BASE_URL}/${slug}`, 307);
+  return Response.redirect(redirectUrl, 307);
 }
 
 addEventListener("fetch", async (/** @type {FetchEvent} */ event) => {
