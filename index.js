22ff1c6bbb1e22cabd802a7e76d164c771635770
Griko Nibras <git@griko.id>
2022-06-10 18:00:36 +0700

    feat(src): add nocache search param option

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ async function handler(/** @type {FetchEvent} */ event) {
   }
 
   const slug = url.pathname.split("/")[1];
-  const redirectUrl = await REDIRECTS.get(slug, { cacheTtl: 60 * 5 });
+  const cacheTtl = url.searchParams.has("nocache") ? 0 : 60 * 5;
+  const redirectUrl = await REDIRECTS.get(slug, { cacheTtl });
   if (!redirectUrl) return Response.redirect(`${BASE_URL}/${slug}`, 307);
   return Response.redirect(redirectUrl, 307);
 }
