e3a1647b1be20cc45d67a7f40baaaf32913a5d51
Griko Nibras <git@griko.id>
2022-06-10 18:15:01 +0700

    fix(src): use valid cache ttl value

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ async function handler(/** @type {FetchEvent} */ event) {
   }
 
   const slug = url.pathname.split("/")[1];
-  const cacheTtl = url.searchParams.has("nocache") ? 0 : 60 * 5;
-  const redirectUrl = await REDIRECTS.get(slug, { cacheTtl });
+  const redirectUrl = await REDIRECTS.get(slug, url.searchParams.has("nocache") ? {} : { cacheTtl: 60 * 5 });
   if (!redirectUrl) return Response.redirect(`${BASE_URL}/${slug}`, 307);
   return Response.redirect(redirectUrl, 307);
 }
